bfc80d82c5130906e6a19043716731fb2e95c4a5
Kilian Beraud <kilianberaud3@gmail.com>
2023-10-05 21:18:59 +0200

    FLOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-//code de base
 const express = require('express')
-  
+const { success } = require('./helper.js')
+let pokemons = require('./mock-pokemon')
+ 
 const app = express()
 const port = 3000
-let pokemons = require(`./mock-pokemon`)
-const { success } = require(`./helper.js`)
-
-app.get('/', (req, res) => res.send('<h1>Hello, Ezaxezcxxpress!</h1>'))
-
-app.get('/api/pokemons/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    const pokemon = pokemons.find(pokemons => pokemons.id === id)
-    const message = `Un pokémon a bien été trouvé`
-    res.json(success(message, pokemon))
+ 
+app.get('/', (req,res) => res.send('Hello again, Express !'))
+ 
+// On retourne la liste des pokémons au format JSON, avec un message :
+app.get('/api/pokemons', (req, res) => {
+  const message = 'La liste des pokémons a bien été récupérée.'
+  res.json(success(message, pokemons)) 
 })
-  
-app.get('/api/pokemons', (req,res) => {
-    res.send(`Il y a ${pokemons.length} pokémons`)
+ 
+app.get('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+  const message = 'Un pokémon a bien été trouvé.'
+  res.json(success(message, pokemon))
 })
-
-
+ 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
-
-//fin de la base
